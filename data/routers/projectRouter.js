94c9cbaa4fd9328c1f6f2bbc9ecf6fc29ec7c915
Juana Villalpando <juana.v14@gmail.com>
2020-03-13 09:35:52 -0700

    create endpoints for action router

---
+++ data/routers/projectRouter.js
@@ -0,0 +1,4 @@
+const router = require('express').Router();
+const Projects = require('../helpers/projectModel');
+
+module.exports = router;
--- server.js
+++ server.js
@@ -10,8 +10,14 @@ server.use(morgan('dev'));
 server.use(cors());
 server.use(express.json());
 
+const actionRouter = require('./data/routers/actionRouter');
+const projectionRouter = require('./data/routers/projectRouter');
+
 server.get('/', (req, res) =>{
     res.status(200).json({ message: 'Server is working, now time to work :)'})
 })
+
+server.use('/api/actions', actionRouter)
+server.use('/api/projects', projectionRouter)
 
 module.exports = server;
